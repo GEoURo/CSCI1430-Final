aa975c9ecedb1e18e768e74817b9c8dd5a4d022e
Jerry <gjerrys@outlook.com>
2022-11-29 12:04:28 -0500

    integrate positional encoding

--- code/encoding.py
+++ code/encoding.py
@@ -3,22 +3,19 @@ import torch.nn as nn
 
 
 def getEncoder(
-    multires: int = 3,
-    i : int = 0):
+        num_freq: int = 3,
+        log_scale: bool = True,
+        i: int = 0
+):
     if i == -1:
         return nn.Identity(), 3
 
-    encoding_kwargs = {
-        'input_channels': 3,
-        'num_freqs': multires,
-        'log_state': True
-    }
+    encoder = PositionalEncoding(num_freq, log_scale=log_scale)
 
-    encoding_obj = PositionalEncoding(**encoding_kwargs)
+    def encode(x):
+        encoder.encode(x)
 
-    embed = lambda x: encoding_obj.embed(x)
-    return embed, encoding_obj.getOutputChannels()
-
+    return encode, encoder.getOutputChannels()
 
 
 class PositionalEncoding:
@@ -26,28 +23,27 @@ class PositionalEncoding:
     Implement the forward pass of the positional encoding, which encode x to (x, sin(2^k * x), cos(2^k * x), ...)
     Args:
         input_channels: the number of channels of the input (3 for xyz and direction)
-        num_freqs: `L_d=4` for viewing direction, `L_x=10` for 3D coordinates
+        num_freq: `L_d=4` for viewing direction, `L_x=10` for 3D coordinates
         log_scale: whether to use log scale for frequency bands,
             `log_scale=True` for generating 0-9 first, and take the power of 2 separately
             `log_scale=False` for taking the power of 2 first, and split equally
 
     """
 
-    def __init__(self, input_channels, num_freqs, log_scale=True, **kwargs):
+    def __init__(self, num_freq, input_channels=3, log_scale=True):
         super(PositionalEncoding, self).__init__()
-        
-        self.num_freqs = num_freqs
+
+        self.num_freq = num_freq
         self.input_channels = input_channels
         self.encode_func = [torch.sin, torch.cos]
 
-
-        self.output_channels = input_channels * (len(self.encode_func) * num_freqs + 1)
+        self.output_channels = input_channels * (len(self.encode_func) * num_freq + 1)
         if log_scale:
-            self.freq_bands = 2**torch.linspace(0, num_freqs - 1, num_freqs)
+            self.freq_bands = 2 ** torch.linspace(0, num_freq - 1, num_freq)
         else:
-            self.freq_bands = torch.linspace(1, 2**(num_freqs - 1), num_freqs)
+            self.freq_bands = torch.linspace(1, 2 ** (num_freq - 1), num_freq)
 
-    def embed(self, x):
+    def encode(self, x):
         """
         Inputs:
             x: (ray_cnt,num_samples, input_channels)
@@ -65,5 +61,3 @@ class PositionalEncoding:
 
     def getInputChannels(self):
         return self.input_channels
-
-    
