9e8f67cc9756166919a9263bc8125eeebc18d214
Jerry <gjerrys@outlook.com>
2022-12-03 16:10:59 -0500

    update script

--- script/run_GRID_GPU.sh
+++ script/run_GRID_GPU.sh
@@ -9,25 +9,26 @@
 #$ -l gpus=1
 
 # This loads the virtual environment
-source /course/cs1430/tf_gpu/bin/activate
+cd ~/final/code
+source ~/GEoURo/bin/activate
 FILE=$1
 # Print out the python file so we have a record. 
 # Useful when iterating a file so you can keep track of what 
 # You are doing
 # uncomment this next line to print the python file to stdout
-# cat $FILE
+echo $FILE
 # Shift increments the argv values.
 shift
 # Tensorpack specific setting to cache ZMQ pipes in /ltmp/ 
 # for speed boost
-export TENSORPACK_PIPEDIR=/ltmp/
+#export TENSORPACK_PIPEDIR=/ltmp/
 # If you need to compile any CUDA kernels do it on the local FS 
 # so it happens faster
-export CUDA_CACHE_PATH=/ltmp/
+#export CUDA_CACHE_PATH=/ltmp/
 # Runs the python file passing all args and pipes n into the file. 
 # -u tells python to not buffer the output to so it is printed
 # more often.
-echo -e "n\n" | python -u $FILE $@
+python -u $FILE $@
 
 #Reminder
 # qhost to look at machines and the current usage
